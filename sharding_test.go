fb9b08f9851507b045e8b9d7c37d0d10388e5b6b
tung.tq <tung.tq@teko.vn>
2024-03-27 14:20:47 +0700

    Add 3 Nodes

--- sharding_test.go
+++ sharding_test.go
@@ -10,6 +10,7 @@ import (
 
 const client1 curator.FakeClientID = "client1"
 const client2 curator.FakeClientID = "client2"
+const client3 curator.FakeClientID = "client3"
 const initClient curator.FakeClientID = "init"
 
 const parentPath = "/sharding"
@@ -131,4 +132,47 @@ func TestSharding_Two_Nodes(t *testing.T) {
 
 	assert.Equal(t, "node02", children[1].Name)
 	assert.Equal(t, `{"shards":[4,5,6,7]}`, string(children[1].Data))
+}
+
+func TestSharding_Three_Nodes(t *testing.T) {
+	store := initStore()
+
+	startSharding(store, client1, "node01")
+	startSharding(store, client2, "node02")
+	startSharding(store, client3, "node03")
+
+	store.Begin(client1)
+	store.Begin(client2)
+	store.Begin(client3)
+
+	initContainerNodes(store, client1)
+	initContainerNodes(store, client2)
+	initContainerNodes(store, client3)
+
+	lockGranted(store, client1)
+	lockBlocked(store, client2)
+	lockBlocked(store, client3)
+
+	store.ChildrenApply(client1)
+	store.CreateApply(client1)
+	store.CreateApply(client1)
+	store.CreateApply(client1)
+
+	store.PrintData()
+	store.PrintPendingCalls()
+
+	lockNode := store.Root.Children[0]
+	assert.Equal(t, "assigns", lockNode.Children[2].Name)
+	children := lockNode.Children[2].Children
+
+	assert.Equal(t, 3, len(children))
+
+	assert.Equal(t, "node01", children[0].Name)
+	assert.Equal(t, `{"shards":[0,1,2]}`, string(children[0].Data))
+
+	assert.Equal(t, "node02", children[1].Name)
+	assert.Equal(t, `{"shards":[3,4,5]}`, string(children[1].Data))
+
+	assert.Equal(t, "node03", children[2].Name)
+	assert.Equal(t, `{"shards":[6,7]}`, string(children[2].Data))
 }
