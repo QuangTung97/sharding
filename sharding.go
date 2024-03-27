1c5347b8261c8854ea4f9b24847856e164cb79f9
tung.tq <tung.tq@teko.vn>
2024-03-27 14:15:17 +0700

    Allocate Shards to Nodes

--- sharding.go
+++ sharding.go
@@ -3,6 +3,9 @@ package sharding
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"slices"
 
 	"github.com/QuangTung97/zk"
 	"github.com/QuangTung97/zk/concurrency"
@@ -12,6 +15,7 @@ import (
 type Sharding struct {
 	parentPath string
 	nodeID     string
+	numShards  ShardID
 
 	cur *curator.Curator
 
@@ -21,9 +25,14 @@ type Sharding struct {
 }
 
 type sessionState struct {
-	lockNodeCreated bool
-	nodesCreated    bool
-	leaderStarted   bool
+	lockCreated    bool
+	nodesCreated   bool
+	assignsCreated bool
+	leaderStarted  bool
+
+	nodes            []string
+	currentAssigns   []string
+	currentAssignMap map[string][]ShardID
 }
 
 func NewNodeID() string {
@@ -35,10 +44,11 @@ func NewNodeID() string {
 	return hex.EncodeToString(data[:])
 }
 
-func New(parentPath string, nodeID string) *Sharding {
+func New(parentPath string, nodeID string, numShards ShardID) *Sharding {
 	s := &Sharding{
 		parentPath: parentPath,
 		nodeID:     nodeID,
+		numShards:  numShards,
 	}
 
 	lock := concurrency.NewLock(s.getLockPath(), nodeID)
@@ -60,6 +70,10 @@ func (s *Sharding) getNodesPath() string {
 	return s.parentPath + "/nodes"
 }
 
+func (s *Sharding) getAssignsPath() string {
+	return s.parentPath + "/assigns"
+}
+
 func (s *Sharding) createContainerNodes(sess *curator.Session, next func(sess *curator.Session)) {
 	s.state = &sessionState{}
 	s.lockBegin = next
@@ -67,7 +81,7 @@ func (s *Sharding) createContainerNodes(sess *curator.Session, next func(sess *c
 }
 
 func (s *Sharding) createCompleted(sess *curator.Session) {
-	if s.state.lockNodeCreated && s.state.nodesCreated {
+	if s.state.lockCreated && s.state.nodesCreated && s.state.assignsCreated {
 		if s.state.leaderStarted {
 			panic("Leader already started")
 		}
@@ -79,16 +93,43 @@ func (s *Sharding) createCompleted(sess *curator.Session) {
 func (s *Sharding) createInitNodes(sess *curator.Session) {
 	sess.Run(func(client curator.Client) {
 		client.Create(s.getLockPath(), nil, 0, func(resp zk.CreateResponse, err error) {
-			s.state.lockNodeCreated = true
+			if err != nil {
+				if errors.Is(err, zk.ErrNodeExists) {
+					s.state.lockCreated = true
+					s.createCompleted(sess)
+					return
+				}
+				panic(err)
+			}
+			s.state.lockCreated = true
 			s.createCompleted(sess)
 		})
 		client.Create(s.getNodesPath(), nil, 0, func(resp zk.CreateResponse, err error) {
 			if err != nil {
+				if errors.Is(err, zk.ErrNodeExists) {
+					s.createEphemeralNode(sess)
+					return
+				}
 				panic(err)
 			}
 			s.createEphemeralNode(sess)
 		})
+		client.Create(s.getAssignsPath(), nil, 0, func(resp zk.CreateResponse, err error) {
+			if err != nil {
+				if errors.Is(err, zk.ErrNodeExists) {
+					s.assignsCreated(sess)
+					return
+				}
+				panic(err)
+			}
+			s.assignsCreated(sess)
+		})
 	})
+}
+
+func (s *Sharding) assignsCreated(sess *curator.Session) {
+	s.state.assignsCreated = true
+	s.createCompleted(sess)
 }
 
 func (s *Sharding) createEphemeralNode(sess *curator.Session) {
@@ -98,16 +139,127 @@ func (s *Sharding) createEphemeralNode(sess *curator.Session) {
 			zk.FlagEphemeral,
 			func(resp zk.CreateResponse, err error) {
 				if err != nil {
+					if errors.Is(err, zk.ErrNodeExists) {
+						s.nodesCreated(sess)
+						return
+					}
 					panic(err)
 				}
-				s.state.nodesCreated = true
-				s.createCompleted(sess)
+				s.nodesCreated(sess)
 			},
 		)
 	})
 }
 
+func (s *Sharding) nodesCreated(sess *curator.Session) {
+	s.state.nodesCreated = true
+	s.createCompleted(sess)
+}
+
 func (s *Sharding) onLeaderCallback(sess *curator.Session, _ func(sess *curator.Session)) {
+	s.leaderStarted(sess)
+}
+
+func (s *Sharding) leaderStarted(sess *curator.Session) {
+	sess.Run(func(client curator.Client) {
+		client.ChildrenW(s.getNodesPath(), func(resp zk.ChildrenResponse, err error) {
+			if err != nil {
+				panic(err)
+			}
+
+			s.state.nodes = resp.Children
+			slices.Sort(s.state.nodes)
+
+			s.handleNodesChanged(sess)
+		}, func(ev zk.Event) {
+		})
+	})
+}
+
+func (s *Sharding) handleNodesChanged(sess *curator.Session) {
+	n := ShardID(len(s.state.nodes))
+	minShardsPerNode := s.numShards / n
+	maxShardsPerNode := (s.numShards + n - 1) / n
+	numMax := s.numShards - minShardsPerNode*n
+
+	nodes := make([]string, n)
+	copy(nodes, s.state.nodes)
+	slices.SortStableFunc(nodes, func(a, b string) int {
+		assignA := s.state.currentAssignMap[a]
+		assignB := s.state.currentAssignMap[b]
+		return len(assignB) - len(assignA)
+	})
+
+	freeShards := map[ShardID]struct{}{}
+	for id := 0; id < int(s.numShards); id++ {
+		freeShards[ShardID(id)] = struct{}{}
+	}
+
+	for _, list := range s.state.currentAssignMap {
+		for _, id := range list {
+			delete(freeShards, id)
+		}
+	}
+
+	for i := 0; i < int(numMax); i++ {
+		s.updateIfChanged(sess, nodes[i], int(maxShardsPerNode), freeShards)
+	}
+
+	for i := int(numMax); i < int(n); i++ {
+		s.updateIfChanged(sess, nodes[i], int(minShardsPerNode), freeShards)
+	}
+}
+
+type ShardID uint32
+
+type assignData struct {
+	Shards []ShardID `json:"shards"`
+}
+
+func freeShardsToList(freeShards map[ShardID]struct{}) []ShardID {
+	list := make([]ShardID, 0, len(freeShards))
+	for id := range freeShards {
+		list = append(list, id)
+	}
+	slices.Sort(list)
+	return list
+}
+
+func (s *Sharding) updateIfChanged(
+	sess *curator.Session,
+	nodeID string, expectLen int, freeShards map[ShardID]struct{},
+) {
+	current := s.state.currentAssignMap[nodeID]
+	if len(current) > expectLen {
+		return
+	}
+	if len(current) < expectLen {
+		list := freeShardsToList(freeShards)
+		missing := expectLen - len(current)
+		current = append(current, list[:missing]...)
+		for _, id := range list[:missing] {
+			delete(freeShards, id)
+		}
+		s.upsertAssigns(sess, nodeID, current)
+		return
+	}
+}
+
+func (s *Sharding) upsertAssigns(sess *curator.Session, nodeID string, shards []ShardID) {
+	pathVal := s.getAssignsPath() + "/" + nodeID
+	data, err := json.Marshal(assignData{
+		Shards: shards,
+	})
+	if err != nil {
+		panic(err)
+	}
+	sess.Run(func(client curator.Client) {
+		client.Create(pathVal, data, 0, func(resp zk.CreateResponse, err error) {
+			if err != nil {
+				panic(err)
+			}
+		})
+	})
 }
 
 func (s *Sharding) GetCurator() *curator.Curator {
--- sharding_test.go
+++ sharding_test.go
@@ -13,6 +13,7 @@ const client2 curator.FakeClientID = "client2"
 const initClient curator.FakeClientID = "init"
 
 const parentPath = "/sharding"
+const numShards = 8
 
 func initStore() *curator.FakeZookeeper {
 	store := curator.NewFakeZookeeper()
@@ -43,7 +44,7 @@ func startSharding(
 	nodeID string,
 ) {
 	factory := curator.NewFakeClientFactory(store, client)
-	s := New(parentPath, nodeID)
+	s := New(parentPath, nodeID, numShards)
 	factory.Start(s.GetCurator())
 }
 
@@ -53,16 +54,81 @@ func TestSharding_Begin(t *testing.T) {
 	startSharding(store, client1, "node01")
 
 	store.Begin(client1)
-	store.CreateApply(client1)
-	store.CreateApply(client1)
-	store.CreateApply(client1)
+	store.CreateApply(client1) // create lock
+	store.CreateApply(client1) // create nodes
+	store.CreateApply(client1) // create assigns
+	store.CreateApply(client1) // create nodes/node01
 
 	// Lock Start
 	store.ChildrenApply(client1)
 	store.CreateApply(client1)
 	store.ChildrenApply(client1)
 
-	assert.Equal(t, []string{
-		"children",
-	}, store.PendingCalls(client1))
+	store.ChildrenApply(client1)
+	store.CreateApply(client1)
+
+	store.PrintData()
+	store.PrintPendingCalls()
+
+	lockNode := store.Root.Children[0]
+	assert.Equal(t, "assigns", lockNode.Children[2].Name)
+	children := lockNode.Children[2].Children
+	assert.Equal(t, 1, len(children))
+	assert.Equal(t, "node01", children[0].Name)
+	assert.Equal(t, `{"shards":[0,1,2,3,4,5,6,7]}`, string(children[0].Data))
+}
+
+func initContainerNodes(store *curator.FakeZookeeper, client curator.FakeClientID) {
+	store.CreateApply(client) // create lock
+	store.CreateApply(client) // create nodes
+	store.CreateApply(client) // create assigns
+	store.CreateApply(client) // create nodes/node01
+}
+
+func lockGranted(store *curator.FakeZookeeper, client curator.FakeClientID) {
+	store.ChildrenApply(client)
+	store.CreateApply(client)
+	store.ChildrenApply(client)
+}
+
+func lockBlocked(store *curator.FakeZookeeper, client curator.FakeClientID) {
+	store.ChildrenApply(client)
+	store.CreateApply(client)
+	store.ChildrenApply(client)
+	store.GetApply(client)
+}
+
+func TestSharding_Two_Nodes(t *testing.T) {
+	store := initStore()
+
+	startSharding(store, client1, "node01")
+	startSharding(store, client2, "node02")
+
+	store.Begin(client1)
+	store.Begin(client2)
+
+	initContainerNodes(store, client1)
+	initContainerNodes(store, client2)
+
+	lockGranted(store, client1)
+	lockBlocked(store, client2)
+
+	store.ChildrenApply(client1)
+	store.CreateApply(client1)
+	store.CreateApply(client1)
+
+	store.PrintData()
+	store.PrintPendingCalls()
+
+	lockNode := store.Root.Children[0]
+	assert.Equal(t, "assigns", lockNode.Children[2].Name)
+	children := lockNode.Children[2].Children
+
+	assert.Equal(t, 2, len(children))
+
+	assert.Equal(t, "node01", children[0].Name)
+	assert.Equal(t, `{"shards":[0,1,2,3]}`, string(children[0].Data))
+
+	assert.Equal(t, "node02", children[1].Name)
+	assert.Equal(t, `{"shards":[4,5,6,7]}`, string(children[1].Data))
 }
